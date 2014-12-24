80a3a17b66cff5a4e600425a79e0ad7371672f77
Snuzzo <jrracinfan@gmail.com>
2014-12-24 11:47:12 -0500

    Revert "Telecomm: update blacklist activity name"
    This reverts commit 333a5f45e791f4f37f8919a45b2f33891a20e5d5.

--- src/com/android/server/telecom/BlacklistCallNotifier.java
+++ src/com/android/server/telecom/BlacklistCallNotifier.java
@@ -107,7 +107,7 @@ class BlacklistCallNotifier extends CallsManagerListenerBase {
         // Get the intent to open Blacklist settings if user taps on content ready
         Intent intent = new Intent(Intent.ACTION_MAIN);
         intent.setClassName("com.android.settings",
-                "com.android.settings.blacklist.BlacklistSettings");
+                "com.android.settings.Settings$BlacklistSettingsActivity");
         PendingIntent blSettingsIntent = PendingIntent.getActivity(mContext, 0, intent, 0);
 
         // Start building the notification
